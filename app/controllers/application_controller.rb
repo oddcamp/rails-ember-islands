93780ca4d3c93a4a3d95828f732e624384bf735d
Filippos Vasilakis <vasilakisfil@gmail.com>
2017-01-22 16:49:08 +0100

    Make logged in user permanent

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
+  before_filter :log_in_test_user
+
   private
 
     # Confirms a logged-in user.
@@ -11,5 +13,10 @@ class ApplicationController < ActionController::Base
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
+    end
+
+    def log_in_test_user
+      @current_user = User.first
+      flash[:info] = "This is a test app and we have assigned you a test user :)"
     end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,6 +18,8 @@ module SessionsHelper
 
   # Returns the user corresponding to the remember token cookie.
   def current_user
+    return @current_user if defined?(@current_user)
+
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
